0e6aa5fcc0c90c8ece868a80f7f1815b5496538e
Chai Feng <chaifeng@chaifeng.com>
2015-05-31 11:33:50 +0800

    enable zh_CN & en_US locale on raspberrypi

--- 00-timezone-locale.sh
+++ 00-timezone-locale.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-locale --all-locales | fgrep zh_CN.utf8 > /dev/null || locale-gen zh_CN.utf8
+if [[ -f /etc/locale.gen ]]; then
+    grep -F '# zh_CN.UTF-8 UTF-8' /etc/locale.gen && \
+        sed -i.zhcn \
+            -e '/# zh_CN.UTF-8 UTF-8/czh_CN.UTF-8 UTF-8' \
+            -e '/# en_US.UTF-8 UTF-8/cen_US.UTF-8 UTF-8' \
+            /etc/locale.gen
+fi
+
+locale --all-locales | fgrep zh_CN.utf8 > /dev/null || locale-gen zh_CN.utf8 en_US.UTF-8
