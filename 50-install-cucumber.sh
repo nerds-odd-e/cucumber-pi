44597c315dcf168273d1ad231c593f44b59c1a75
Chai Feng <chaifeng@chaifeng.com>
2015-06-01 23:42:11 +0800

    use default gemset for rvm

--- 50-install-cucumber.sh
+++ 50-install-cucumber.sh
@@ -12,7 +12,7 @@ su -lc /bin/bash pi <<EOF
   rvm requirements
   grep '.rvm/scripts/rvm' \$HOME/.bashrc || echo '[[ -s "\$HOME/.rvm/scripts/rvm" ]] && source "\$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*' >> \$HOME/.bashrc
   rvm list | fgrep ruby || LC_ALL=C DEBIAN_FRONTEND=noninteractive rvm --quiet-curl install ruby
-  rvm use ruby@wordpress --create --default
+  rvm use ruby --default
   grep ^gem: \$HOME/.gemrc || ( echo 'gem: --no-rdoc --no-ri' | tee -a \$HOME/.gemrc )
   gem sources --remove https://rubygems.org/
   gem sources -a https://ruby.taobao.org/
