262492f9c221a4b206cf30db879b078e7e98a3bd
Chai Feng <chaifeng@chaifeng.com>
2015-08-12 22:41:04 +0800

    extract variables for wordpress

--- roles/wordpress/files/wp-cli.sh
+++
@@ -1,2 +0,0 @@
-export WP_CLI_CONFIG_PATH=/opt/wp-cli/config.yml
-export PATH=/opt/wp-cli:$PATH
---
+++ roles/wordpress/templates/wp-cli.sh
@@ -0,0 +1,2 @@
+export WP_CLI_CONFIG_PATH={{ wp_cli_home }}/config.yml
+export PATH={{ wp_cli_home }}:$PATH
