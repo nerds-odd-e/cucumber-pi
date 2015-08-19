e10da8801e0b6d0c1c7ac7437bb1c90bb8f71071
Chai Feng <chaifeng@chaifeng.com>
2015-08-19 14:55:07 +0800

    closing comment for post 1-9

--- roles/wordpress/templates/install_wordpress.sh
+++ roles/wordpress/templates/install_wordpress.sh
@@ -50,7 +50,7 @@ $WP_CLI_CMD option update comments_notify   ''
 $WP_CLI_CMD option update moderation_notify ''
 $WP_CLI_CMD option update show_avatars      ''
 $WP_CLI_CMD option update permalink_structure  "/%postname%/"
-$WP_CLI_CMD post update 1 6 7 8 9 --comment_status=closed
-$WP_CLI_CMD post update 1 6 7 8 9 --ping_status=closed
+$WP_CLI_CMD post update 1 2 3 4 5 6 7 8 9 --comment_status=closed
+$WP_CLI_CMD post update 1 2 3 4 5 6 7 8 9 --ping_status=closed
 
 touch "${WORDPRESS_HOME}/odd-e.txt"
