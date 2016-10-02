93c1ea71776544a3c0f129af8b11dd4ee5b98927
Chai Feng <chaifeng@chaifeng.com>
2016-10-02 13:55:31 +0800

    changing wordpress links

--- roles/wordpress/files/wordpress_reset_config.sh
+++ roles/wordpress/files/wordpress_reset_config.sh
@@ -12,6 +12,7 @@ $WP_CLI_CMD option update comment_whitelist     ''
 $WP_CLI_CMD option update comments_notify   ''
 $WP_CLI_CMD option update moderation_notify ''
 $WP_CLI_CMD option update show_avatars      ''
-$WP_CLI_CMD option update permalink_structure  "/%postname%/"
+$WP_CLI_CMD rewrite structure '/%year%/%monthnum%/%postname%'
+$WP_CLI_CMD rewrite flush
 $WP_CLI_CMD post update 1 2 3 4 5 6 7 8 9 --comment_status=closed
 $WP_CLI_CMD post update 1 2 3 4 5 6 7 8 9 --ping_status=closed
