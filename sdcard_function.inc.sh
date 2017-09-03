945741e7f53b62dde57f08cd7ac1f87afdb353fa
Chai Feng <chaifeng@chaifeng.com>
2017-09-03 21:18:04 +0800

    To skip any disk with a capacity of greater than 32GB

--- sdcard_function.inc.sh
+++ sdcard_function.inc.sh
@@ -14,9 +14,19 @@ function contains() {
     return 1
 }
 
-DISK_LIST_READONLY=( $(get_disk_list) )
+function skip_disk() {
+    for _DISK in "$@"; do
+        DISK_LIST_READONLY+=( $_DISK )
+    done
+    echo "Will skip these disks: ${DISK_LIST_READONLY[*]}"
+    echo "Any disk with a capacity of less than 32G will be treated as an SD card."
+}
+
+DISK_LIST_READONLY=( $(get_disk_list physical) )
 contains DISK_LIST_READONLY[@] /dev/disk0
-contains DISK_LIST_READONLY[@] /dev/disk1 || true
+
+skip_disk /dev/disk1 >/dev/null
+contains DISK_LIST_READONLY[@] /dev/disk1
 
 function find_sdcard() {
     local -a _DISKS=( $(get_disk_list physical) )
@@ -35,9 +45,9 @@ function run() {
         echo "$FUN is not a function, exit now."
         exit 2
     fi
-    echo "Will skip these disks: ${DISK_LIST_READONLY[*]}"
+    skip_disk
     while true; do
-        echo -n "Insert a new SD card, and will find it automaticly. Press enter key to exit."$'\r'
+        echo -n $'\r'"Insert a new SD card, and will find it automaticly. Press enter key to exit."
         declare SDCARD
         SDCARD="$(find_sdcard || true)"
         if test -z "$SDCARD"; then
@@ -49,12 +59,12 @@ function run() {
 
         [[ "$SDCARD" == /dev/disk[2-9] ]]
 
-        eval "$@"
+        if eval "$@"; then
+            diskutil eject "$SDCARD"
 
-        diskutil eject "$SDCARD"
-
-        echo "SD card $SDCARD has been rejected."
-        echo "Replace a new SD card now."
+            echo "SD card $SDCARD has been rejected."
+            echo "Replace a new SD card now."
+        fi
         echo ""
     done
 }
